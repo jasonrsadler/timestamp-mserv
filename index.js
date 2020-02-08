fa5f8cbf2579b64ffc64c8df0b53a184b0e2cacd
Jason Sadler <jasonsadler@amplifiedit.com>
2020-02-08 17:53:20 -0500

    complete

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({extended: 'false'}));
+app.use(bodyParser.json());
+
+app.get('/api/timestamp/:date_string', function (req, res, next) {
+  let date
+  if (!req && !req.params && (!req.params.date_string || req.params.date_string === '')) {
+    date = new Date()
+  }
+  date = new Date(req.params.date_string)
+  if (isNaN(date)) {
+    res.json({error: 'Invaid Date'})
+    return
+  }
+  res.json({unix: date.getTime(), utc: date.toUTCString()})
+})
+
+var listener = app.listen(process.env.PORT || 3000 , function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
+
+module.exports = app
